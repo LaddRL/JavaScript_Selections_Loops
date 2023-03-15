26ea9bfd6d238a9ff9a9c27c1908586db4676e3a
ladd lindsay <laddrlindsay@gmail.com>
2023-03-15 09:52:43 -0700

    Finished all exercises

--- app.js
+++ app.js
@@ -26,10 +26,91 @@ for (let i = 1; i <= 100; i++) {
 
 // Exercise 3 Section
 let i = 1;
-const n = 100
-do {
-    
-    console.log(i);
-    i % 2 == 1;
+while (i <= 100) {
+    if (1 % 2 !== 0) {
+        console.log(i);
+    }
     i++;
-} while(i <= n)
+}
+
+i=1
+while (i <= 100) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FIZZBUZZ')
+    }
+    else if (i % 3 === 0) {
+        console.log('FIZZ')
+    }
+    else if (i % 5 === 0) {
+        console.log('BUZZ')
+        i++;
+    }
+}
+i=1
+do {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FIZZBUZZ')
+    }
+    else if (i % 3 === 0) {
+        console.log('FIZZ')
+    }
+    else if (i % 5 === 0) {
+        console.log('BUZZ')
+        i++;
+    }
+    else {
+
+    }
+    i++;
+} while (1<=100);
+
+// Exercise 4 Section
+
+let value = Math.round((Math.random() * 500)); 
+// creates a random number between 0 and 500
+let n = Math.round(Math.random() * (500 - 100) + 100); 
+// creates a random number between 100 and 500
+
+let result;
+
+for (let i = 0; i <= n; i++) {
+    if (i === value) {
+        result = 'Found Value!';
+        break;
+    }
+    else {
+        result = 'Did not find value';
+    }
+
+}
+// Exercise 5 Section
+console.log(result)
+
+
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1); 
+// creates a random number between 1 and 10
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1); 
+// creates a random number between 1 and 10
+let start = Math.round(Math.random() * (10 - 1) + 1); 
+// creates a random number between 1 and 10
+let end = Math.round(Math.random() * (1000 - 100) + 100); 
+// creates a random number between 100 and 1000
+
+console.log(fizzDivisor)
+console.log(buzzDivisor)
+
+for (i = start; i <= end; i++){
+
+
+if (i % fizzDivisor === 0 && i % buzzDivisor === 0) {
+    console.log('FIZZBUZZ')
+
+}
+else if (i % fizzDivisor === 0) {
+    console.log('FIZZ')
+
+}
+else if (i % buzzDivisor===0){ 
+console.log('BUZZ')}
+
+}
